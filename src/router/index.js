37e7ae0b497725e0728369ab2f10e94a00ad2dc1
xhyang2 <xhyang2@chinaums.com>
2020-08-07 17:52:59 +0800

    asset: change logo

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,12 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/logo',
-    name: 'logo',
-    component: () => import(/* webpackChunkName: "logo" */ '../views/test/Logo.vue')
   }
+  // {
+  //   path: '/logo',
+  //   name: 'logo',
+  //   component: () => import(/* webpackChunkName: "logo" */ '../views/test/Logo.vue')
+  // }
 ]
 
 const router = new VueRouter({
